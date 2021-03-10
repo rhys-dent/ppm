df92e8d4537929d972d7289a9a23b50b71db0973
ppm <PrometheusPM@outlook.com>
2021-03-09 21:07:05 -0700

    asdfdd

--- src/common/ServiceNavLink.js
+++ src/common/ServiceNavLink.js
@@ -39,7 +39,7 @@ const BlackBackS = styled.div`
   margin: ${margin};
   position: absolute;
   top: 0;
-  text-shadow: 0 0 0.5px gold;
+  text-shadow: 0 0 0.5px grey;
   padding: calc(${margin} * 2);
   transition: text-shadow 0.125s ease-in-out;
 `;
